9d37bb70411d5b305b23aaf281015f00f94bfb9a
sangeethajyothilingam <sangeethajoe2007@gmail.com>
2023-03-15 10:48:46 +0530

    update

--- src/config.js
+++ src/config.js
@@ -1,4 +1,1 @@
-export const config = { api: "http://localhost:8000" };
-
-// http://localhost:5000
-// https://chat-app-server-phi.vercel.app
+export const config = { api: "https://server-puce-kappa.vercel.app" };
