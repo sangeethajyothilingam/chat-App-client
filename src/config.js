5304f54433e9926375130aeb3d99056a027f8c88
sangeethajyothilingam <sangeethajoe2007@gmail.com>
2023-03-15 09:59:01 +0530

    first commit

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-export const config = { api: "http://localhost:4000" };
+export const config = { api: "http://localhost:8000" };
 
 // http://localhost:5000
 // https://chat-app-server-phi.vercel.app
